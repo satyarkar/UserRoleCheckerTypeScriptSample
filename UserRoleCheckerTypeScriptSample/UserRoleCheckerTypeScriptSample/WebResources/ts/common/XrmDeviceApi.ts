2e6c45c027a1c58695c6f7039729a1f8e9282503
Satya Kar <40472975+satyarkar@users.noreply.github.com>
2019-01-31 11:31:17 +0000

    Add files via upload

---
+++ UserRoleCheckerTypeScriptSample/UserRoleCheckerTypeScriptSample/WebResources/ts/common/XrmDeviceApi.ts
@@ -0,0 +1,35 @@
+﻿namespace KARLAB.Utility.XrmDeviceApi {
+
+     /**
+     * Method to open dilog box(device camera, file browser) and update the entity image using Xrm WebApi
+     * @param {recordId} Guid Entity record Guid
+     * @param {entityType} string Entity logical name
+     */
+    export function CaptureEntityImage(recordId, entityType) {
+        if (recordId == null || entityType == null)
+            return;
+        Xrm.Device.pickFile().then(
+            function success(result) {
+                if (result != null) {
+                    Xrm.WebApi.updateRecord(entityType, recordId, { "entityimage": result[0].fileContent }).then(
+                        function (result) {
+                            Xrm.Navigation.openAlertDialog({ "text": "Done!" }).then(
+                                function () {
+                                    Xrm.Navigation.openForm({ "entityId": recordId, "entityName": entityType, "openInNewWindow": false })
+                                }
+                            );
+                        }
+                        ,
+                        function (error) {
+                            Xrm.Navigation.openErrorDialog({ "message": "We couldn't update the record. Error Message: " + error.message });
+                        }
+                    );
+                }
+            }
+            ,
+            function (error) {
+                Xrm.Navigation.openErrorDialog({ "message": "We couldn't pick the file. Error Message: " + error.message });
+            }
+        );
+    }
+}
